144ecb501e8a4c8d2a3893e5bd3a81999de9544e
hdjsjjdgrhj <hudufysyeur@gmail.com>
2026-01-18 14:43:44 +0800

    Update Skyhub.lua

--- Skyhub.lua
+++ Skyhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfs7xbk28d.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfhleknug4.lua"))()
