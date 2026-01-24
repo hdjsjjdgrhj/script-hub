fb2869c67e62db950faa1af03467908a5a987e36
hdjsjjdgrhj <hudufysyeur@gmail.com>
2026-01-24 18:42:51 +0800

    Update script URL in Skyhub.lua

--- Skyhub.lua
+++ Skyhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obf01hbo2m9.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfm6v3mfk6.lua"))()
