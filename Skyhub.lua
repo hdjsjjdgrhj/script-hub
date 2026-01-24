39cc02c9af2affd2d902f648ccf986b02a44daee
hdjsjjdgrhj <hudufysyeur@gmail.com>
2026-01-24 20:45:06 +0800

    Update Skyhub.lua

--- Skyhub.lua
+++ Skyhub.lua
@@ -1,1 +1,9 @@
+if game.GameId == 7008097940 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obf6yxon5xt.lua"))()
+    return 
+end
+if game.GameId == 2820580801 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obf6yxon5xt.lua"))()
+    return 
+end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfm6v3mfk6.lua"))()
