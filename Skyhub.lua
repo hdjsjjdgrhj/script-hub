c97415f3ec66d6b15b20d4cc2e97f59cb73b8732
hdjsjjdgrhj <hudufysyeur@gmail.com>
2026-06-14 15:20:13 +0800

    Update Skyhub.lua

--- Skyhub.lua
+++ Skyhub.lua
@@ -1,13 +1,9 @@
 if game.GameId == 7008097940 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(3).txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(2).txt"))()
     return 
 end
 if game.GameId == 2820580801 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(3).txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(2).txt"))()
     return 
 end
-if game.GameId == 1494262959 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(1).txt"))()
-    return 
-end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/114514jjsjzjzjzsk/Script/refs/heads/main/obfuscated.lua%20(8).txt"))()
